3c98531befc013378bfa3ff4e6769eaea952b34d
guanyu <sudo4x@gmail.com>
2025-08-11 17:45:19 +0800

    整理侧边菜单

--- src/navigation/sidebar/sidebar-items.ts
+++ src/navigation/sidebar/sidebar-items.ts
@@ -1,24 +1,4 @@
-import {
-  ShoppingBag,
-  Forklift,
-  Mail,
-  MessageSquare,
-  Calendar,
-  Kanban,
-  ReceiptText,
-  Users,
-  Lock,
-  Fingerprint,
-  SquareArrowUpRight,
-  LayoutDashboard,
-  ChartBar,
-  Banknote,
-  Gauge,
-  GraduationCap,
-  TrendingUp,
-  PieChart,
-  type LucideIcon,
-} from "lucide-react";
+import { LayoutDashboard, ChartBar, Banknote, PieChart, type LucideIcon } from "lucide-react";
 
 export interface NavSubItem {
   title: string;
@@ -65,30 +45,6 @@ export const sidebarItems: NavGroup[] = [
         url: "/dashboard/finance",
         icon: Banknote,
       },
-      {
-        title: "分析",
-        url: "/dashboard/analytics",
-        icon: Gauge,
-        comingSoon: true,
-      },
-      {
-        title: "电商",
-        url: "/dashboard/e-commerce",
-        icon: ShoppingBag,
-        comingSoon: true,
-      },
-      {
-        title: "学院",
-        url: "/dashboard/academy",
-        icon: GraduationCap,
-        comingSoon: true,
-      },
-      {
-        title: "物流",
-        url: "/dashboard/logistics",
-        icon: Forklift,
-        comingSoon: true,
-      },
     ],
   },
   {
@@ -100,75 +56,6 @@ export const sidebarItems: NavGroup[] = [
         url: "/investment/portfolios",
         icon: PieChart,
         isNew: true,
-      },
-    ],
-  },
-  {
-    id: 3,
-    label: "页面",
-    items: [
-      {
-        title: "邮件",
-        url: "/mail",
-        icon: Mail,
-        comingSoon: true,
-      },
-      {
-        title: "聊天",
-        url: "/chat",
-        icon: MessageSquare,
-        comingSoon: true,
-      },
-      {
-        title: "日历",
-        url: "/calendar",
-        icon: Calendar,
-        comingSoon: true,
-      },
-      {
-        title: "看板",
-        url: "/kanban",
-        icon: Kanban,
-        comingSoon: true,
-      },
-      {
-        title: "发票",
-        url: "/invoice",
-        icon: ReceiptText,
-        comingSoon: true,
-      },
-      {
-        title: "用户",
-        url: "/users",
-        icon: Users,
-        comingSoon: true,
-      },
-      {
-        title: "角色",
-        url: "/roles",
-        icon: Lock,
-        comingSoon: true,
-      },
-      {
-        title: "认证",
-        url: "/auth",
-        icon: Fingerprint,
-        subItems: [
-          { title: "登录", url: "/auth/v1/login", newTab: true },
-          { title: "注册", url: "/auth/v1/register", newTab: true },
-        ],
-      },
-    ],
-  },
-  {
-    id: 4,
-    label: "其他",
-    items: [
-      {
-        title: "其他",
-        url: "/others",
-        icon: SquareArrowUpRight,
-        comingSoon: true,
       },
     ],
   },
